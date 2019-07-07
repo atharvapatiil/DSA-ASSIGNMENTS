cf3bacbbe08bcd25d6f8f16d2b13efbfdce3b873
sahilhadke <51915783+sahilhadke@users.noreply.github.com>
2019-07-07 22:54:06 +0530

    Update Assignment_1.cpp

--- Assignment_1.cpp
+++ Assignment_1.cpp
@@ -9,7 +9,6 @@
     This assignment can also be used as OOP 1st Assignment.
     Question: Write a code using basic OOP concepts.
 */
-
 #include<iostream>
 using namespace std;
 
@@ -20,7 +19,6 @@ class Calc
 public:
 
     char op;  //operator is declared in public because it is used in switch 
-
     Calc()  //Constructor
     {
 	cout<<"Enter operation to be performed in the format 'number operator number'"<<endl;
@@ -32,7 +30,6 @@ public:
     void Multiply();   //Multiplies 2 numbers
     void Divide();     //Divides 2 numbers
     void Mod();        //Returns the remainder of 2 of division of numbers
-
 }; 
 
 void Calc::Add()
@@ -47,16 +44,7 @@ void Calc::Subtract()
 
 void Calc::Divide()
 {
-    cout<<"Answer: "<<(float)num1/num2<<endl;  
-
-    /*
-      Some integers when divided give answer in points
-      For example: 5/2 = 2.5
-      So if we print num1/num2, we get answer as 2 instead of 2.5, beacause
-      the compiler will be giving result in int form.
-     
-      To avoid this, I used typecasting which considers the float value of num1/num2
-    */
+    cout<<"Answer: "<<(float)num1/num2<<endl; //Typecasting    
 }
 
 void Calc::Mod()
@@ -69,47 +57,39 @@ void Calc::Multiply()
     cout<<"Answer: "<<num1*num2<<endl;
 }
 
-
-
 int main()
 {
-
     int _condition = 1;
-
     do{
-    	Calc obj;    
-	
+    	Calc obj;    	
     	switch(obj.op)
     	{
-
-		case '+':
-	    	    obj.Add();
-	    	    break;
-		case '-':
-	    	    obj.Subtract();
-	    	    break;
-		case '*':
-		    obj.Multiply();
-		    break;
-		case '/':
-		    obj.Divide();
-		    break;
-		case '%':
-		    obj.Mod();
-		    break;
-		default:
-		    cout<<"You Entered wrong character"<<endl;
+	    case '+':
+	        obj.Add();
+	        break;
+	    case '-':
+	       obj.Subtract();
+	       break;
+	    case '*':
+		obj.Multiply();
+		break;
+	    case '/':
+		obj.Divide();
+		break;
+	    case '%':
+		obj.Mod();
+		break;
+	    default:
+		cout<<"You Entered wrong character"<<endl;
         }
 
 	cout<<"Do you want to continue? \n1: Yes\n0: No\n";
 	cin>>_condition;
 
     }while(_condition);
-
    /*
 	1 = true 
 	0 = false
    */
-
     return 0;
 }
