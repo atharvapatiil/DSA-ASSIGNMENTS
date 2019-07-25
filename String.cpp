b5a9dd4084c96a93c4f46e52345f72e3c68218cf
Sahil Hadke <51915783+sahilhadke@users.noreply.github.com>
2019-07-25 15:17:02 +0530

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -2,26 +2,110 @@
 #include<cstring>
 using namespace std;
 
+const int size = 50;
+
 class String
 {
 public:
-	char str[50];	
-	int length(char arr[50]);	
-	void copyString(char arr[50]); 
-	void Reverse();
-	
+	char str[size];	
+	int length(char arr[size]);								//done
+	int length();											//done
+	void copyString(char arr[size], char arr2[size]); 		//done
+	void Reverse(char arr[size]);							//done
+	void Concat(char arr[size], char arr2[size]); 			//done
+	void getData();											//done
+	bool Search(char arr[size]);							//done										
+	bool checkEqual(char arr[size]);						//done
+	bool Palindrome();										//done
 
 };
 
-void String::Reverse()
+bool String::Palindrome()
 {
+	char arr[size];
+	Reverse(str);
+	copyString(arr, str);
+	Reverse(str);
 
+	return(checkEqual(arr));		
 }
 
-int String::length(char arr[50]) //Returns the length of argument
+bool String::checkEqual(char arr[size])
+{	
+	for(int i = 0; i < length(); i++)
+	{
+		if(str[i] != arr[i])
+			return false;
+	}
+	return true;
+}
+
+bool String::Search(char arr[size])
+{
+	int len = length(arr);
+	int i,j,count = 0;
+	for(i = 0; i <= length()-len; i++)
+	{
+		for(j = 0; j < len; j++)
+		{
+			if(str[i + j] != arr[j])		
+				break;
+			
+			count++;			
+		}	
+		if(count == len)		
+			return true;		
+		else		
+			count = 0;		
+	}
+	
+	return false;
+}
+
+void String::Concat(char arr[size], char arr2[size])  //Concates and stores in first argument
+{
+	int i,j;
+	for(i = length(arr), j = 0; i < size; i++, j++)
+	{
+		arr[i] = arr2[j];
+		if(arr[j] == '\0')
+			break;
+	}
+}
+
+void String::Reverse(char arr[size])
+{
+	for(int i = 0; i < length(arr)/2; i++)
+	{
+		char temp = arr[i];
+		arr[i] = arr[length(arr) - 1 - i];
+		arr[length(arr) - 1 - i] = temp;
+	}
+}
+
+void String::getData()  //gets input
+{
+	cout<<"Enter your string:";
+	cin>>str;
+}
+
+int String::length()  //returns the length of this->str
 {
 	int count = 0;
-	for(int i = 0; i < 50; i++)
+	for(int i = 0; i < size; i++)
+	{
+		if(str[i] == '\0')
+			break;
+
+		count++;		
+	}
+	return count;
+}
+
+int String::length(char arr[size]) //Returns the length of argument
+{
+	int count = 0;
+	for(int i = 0; i < size; i++)
 	{
 		if(arr[i] == '\0')
 			break;
@@ -31,11 +115,11 @@ int String::length(char arr[50]) //Returns the length of argument
 	return count;
 }
 
-void String::copyString(char arr[50]) //Copys the argument to the obj.str
+void String::copyString(char arr[size], char arr2[size]) //Copys the argument to the obj.str
 {
-	for(int i = 0; i < 50; i++)
+	for(int i = 0; i < size; i++)
 	{
-		this->str[i] = arr[i];
+		arr[i] = arr2[i];
 		if(arr[i] == '\0')
 			break;
 	}	
@@ -44,37 +128,10 @@ void String::copyString(char arr[50]) //Copys the argument to the obj.str
 
 int main()
 {
-	int condition;
-	cout<<"Enter the operation:";
-	cin>>condition;
-
-	switch(condition)
-	{
-		case 1: //Length
-			String s1;
-			cout<<"\nEnter your string:";
-			cin>>s1.str;
-			cout<<s1.length(s1.str)<<endl;
-			delete s1;
-			break;
-
-		case 2: //CopyString
-			String s1;
-			cout<<"\nEnter your string:";
-			cin>>s1.str;
-			char arr[50];
-			cout<<endl<<"Enter the string you want to copy:";
-			cin>>arr;
-			s1.copyString(arr);
-			cout<<"Copied String is:"
-			cout<<s1.str;
-			delete s1;
-			break;
-
-		case 3
-			
-
-	}
+	String s1;
+	s1.getData();	
+	cout<<s1.Palindrome();
+	
 
 	return 0;
 }
