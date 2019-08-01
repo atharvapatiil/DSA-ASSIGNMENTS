ea9ab922983b13a93199e929e74958328a65dd37
Sahil Hadke <51915783+sahilhadke@users.noreply.github.com>
2019-08-01 15:01:32 +0530

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,3 +1,21 @@
+/*
+
+	========================
+	   SINGLY LINKED LIST
+	========================
+
+	Name : Sahil Hadke
+	Roll Number: 17
+	
+	Department of Computer Engineering has student's club named 'Pinnacle Club'. Students of Second, third and final year of department can be granted membership on request.  Similarly one may cancel the membership of club. First node is reserved for president of club and last node  is  reserved  for  secretary  of  club.  Write  C++  program  to  maintain  club  member‘s information using singlylinked list. Store student PRN and Name. Write functions to 
+	
+a)Add and delete the members as well as president or even secretary.
+b)Compute total number of members of club
+c)Display members 
+d)Display list in reverse order using recursion
+e)Two linked lists existsfor two divisions. Concatenate two lists.
+*/
+
 #include<iostream>
 #include<string>
 using namespace std;
@@ -24,8 +42,49 @@ public:
     int Count();										//Done
     void Display();										//Done
     void ElementAtIndex(int index);						//Done
-    void Remove(int index);
+    void Remove(int index);								//Done
+	void Reverse();										//Done
+	void Concat(List &l2);								//Done
 };
+
+void List::Concat(List &l2)
+{
+	Node *current1 = first;
+	Node *current2 = l2.first;
+
+	if(first == NULL) //l1 is Empty
+	{
+		cout<<"The first list is empty"<<endl;
+		return;
+	}
+	else if(l2.first == NULL) //l2 is Empty
+	{
+		cout<<"The second list is empty"<<endl;
+		return;
+	}
+	else
+	{
+		while(current1->next != NULL)
+		{
+			current1=current1->next;
+		}
+		current1->next = current2;		
+	}
+}
+
+void List::Reverse()
+{
+	int _count = 0, _count2 = 0;	
+	for(int i = 0; i < Count(); i++)
+	{	
+		Node *current = first;
+		for(int j = Count() - i; j > 1; j--)
+		{
+			current=current->next;
+		}  		
+		cout<<"PRN: "<<current->prn<<" Name: "<<current->name<<endl;
+	}		
+}
 
 void List::Remove(int index)
 {
@@ -42,8 +101,7 @@ void List::Remove(int index)
 		while(_count <= index)
 		{
 			current = current->next;
-		}
-		
+		}		
 	}
 	else
 	{
@@ -62,7 +120,6 @@ void List::Remove(int index)
 		previous->next = later;
 		delete current;
 	}
-
 }
 
 void List::ElementAtIndex(int index)
@@ -194,11 +251,42 @@ int List::Count()
 int main()
 {
 	List l1;
-	l1.InsertAtEnd(1, "Sahil Hadke");
-	l1.InsertAtEnd(2, "Roll two");
-	l1.InsertAtStart(0, "Roll zero");
-	l1.InsertInMiddle(5, "Second element", 1);
-	cout<<l1.Count();
+	cout<<"List l1 created!"<<endl;
+	cout<<"Displaying list: "<<endl;
 	l1.Display();
+	cout<<endl;
+
+	l1.InsertAtEnd(2, "Roll Number Two"); 
+	l1.InsertAtEnd(4, "Roll Number Four"); 
+	cout<<"Inserted 2 and 4 Roll numbers at end."<<endl;
+	cout<<"Displaying list: "<<endl;
+	l1.Display();
+	cout<<endl;
+	
+	l1.InsertInMiddle(3, "Roll Number Three", 1); //At index 1
+	l1.InsertAtStart(1, "Roll Number One");
+	cout<<"Inserted roll no. 3 at index 1 and then inserted roll no. 1 at start"<<endl;
+	cout<<"Displying list: "<<endl;
+	l1.Display();
+	cout<<endl;
+
+	List l2;
+	cout<<"Created List l2"<<endl;	
+	l2.InsertAtEnd(5, "Roll Number Five");
+	l2.InsertAtEnd(6, "Roll Number Six");
+	cout<<"Displaying list l2: "<<endl;
+	l2.Display();
+	cout<<endl;
+
+	cout<<"Joining two lists and displaying: "<<endl;
+	l1.Concat(l2);
+	l1.Display();
+	cout<<endl;
+
+	cout<<"Removing Roll number 2 at index 1 and displaying."<<endl;
+	l1.Remove(1);
+	l1.Display();
+	cout<<endl<<"Code by Sahil Hadke";
+
     return 0;
 }
