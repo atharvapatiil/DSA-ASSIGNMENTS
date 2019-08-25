637377ba5b68740c7ca779c7969e97c740dc90ae
Sahil Hadke <51915783+sahilhadke@users.noreply.github.com>
2019-08-25 22:16:46 +0530

    Positive-Negative Numbers

--- Assignment_9.cpp
+++ Assignment_9.cpp
@@ -26,9 +26,9 @@ public:
 
 class List
 {
-	Node *head, *head1, *head2;
-
 public:
+
+    Node *head, *head1, *head2;
 
 	List()
 	{
@@ -38,9 +38,11 @@ public:
 	}
 
 	void Insert();
-	void Display();
+	void Display(Node *s);
 	void DeleteNegative();
 	void Sort();
+	void Split();
+	void Merge();
 };
 
 void List::Insert()
@@ -66,21 +68,22 @@ void List::Insert()
 	}
 }
 
-void List::Display()
-{    
-    if(head == NULL)
+void List::Display(Node *s)
+{
+    if(s == NULL)
     {
         cout<<"EMPTY LIST"<<endl;
     }
     else
     {
 		Node *current;
-    	current = head;
+    	current = s;
         while(current != NULL)
         {
-            cout<<current->num<<endl;
+            cout<<current->num<<" ";
             current = current->next;
         }
+        cout<<endl;
     }
 }
 
@@ -99,7 +102,7 @@ void List::Sort()
 	else
 	{
 		Node *temp1 = head, *temp2 = head;
-		
+
 		while(temp1->next != NULL)
 		{
 			while(temp2->next != NULL )
@@ -109,7 +112,7 @@ void List::Sort()
 				{
 					int temp = temp2->num;
 					temp2->num = temp1->num;
-					temp1->num = temp; 		
+					temp1->num = temp;
 				}
 			}
 			temp1 = temp1->next;
@@ -118,34 +121,99 @@ void List::Sort()
 		return;
 	}
 }
-/*
+
 void List::DeleteNegative()
 {
-	if(head == NULL)
-    {
-        cout<<"EMPTY LIST"<<endl;
-    }
-	else
+	Sort();
+
+	Node *temp = head, *temp2;
+
+	while(temp->num < 0)
 	{
-		Node *pred = head, *current = head, *succ = head;
-		while(pred->next != NULL)
-		{
-			if(current->num < 0)
-				delete current;
-		}
+
+        temp2 = temp->next;
+        delete temp;
+        temp = temp2;
+        if(temp->num > 0)
+            head = temp;
+        temp = temp->next;
 	}
 }
-*/
+
+void List::Split()
+{
+    Sort();
+
+    head1 = head;
+    Node *temp = head, *prev = head;
+    while(temp->num < 0)
+    {
+        if((prev->next)->num < 0)
+            prev = prev->next;
+        temp = temp->next;
+    }
+    head2 = temp;
+    prev->next = NULL;
+    head = NULL;
+}
+
+void List::Merge()
+{
+    head = head1;
+    Node *temp = head;
+    while(temp->next != NULL)
+        temp = temp->next;
+    temp->next = head2;
+}
+
 int main()
 {
 	List l1;
-	l1.Insert();
-	l1.Insert();
-	l1.Insert();
-	l1.Insert();
 
-	l1.Sort();
-	l1.Display();	
+	int count, condition = 1;
+	cout<<"Enter number of times you want to insert data :";
+	cin>>count;
+
+	for(int i = 0; i < count; i++)
+    {
+        l1.Insert();
+    }
+    int choice;
+
+    do
+    {
+        cout<<"1 - Display\n2 - Split\n3 - Merge\n4 - Delete Negative\n5 - Sort\nEnter choice here :";
+        cin>>choice;
+
+        switch(choice)
+        {
+            case 1:
+                cout<<"Starting from head :";
+                l1.Display(l1.head);
+                cout<<"Starting from head1 :";
+                l1.Display(l1.head1);
+                cout<<"Starting from head2 :";
+                l1.Display(l1.head2);
+                break;
+            case 2:
+                l1.Split();
+                break;
+            case 3:
+                l1.Merge();
+                break;
+            case 4:
+                l1.DeleteNegative();
+                break;
+            case 5:
+                l1.Sort();
+                break;
+            default:
+                cout<<"Wrong Input"<<endl;
+        }
+
+        cout<<"Enter 1 to Continue, 0 to exit :";
+        cin>>condition;
+    }while(condition);
 
 	return 0;
 }
