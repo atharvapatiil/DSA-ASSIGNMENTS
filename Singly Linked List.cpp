6097db358be32346a8495b0dcda48d99c0910712
Sahil Hadke <51915783+sahilhadke@users.noreply.github.com>
2019-08-25 10:02:52 +0530

    Update Singly Linked List.cpp

--- Singly Linked List.cpp
+++ Singly Linked List.cpp
@@ -1,19 +1,25 @@
 /*
 
-	========================
-	   SINGLY LINKED LIST
-	========================
+	======================================
+	   ASSUGNMENT 7 : SINGLY LINKED LIST
+	======================================
 
-	Name : Sahil Hadke
-	Roll Number: 17
-	
-	Department of Computer Engineering has student's club named 'Pinnacle Club'. Students of Second, third and final year of department can be granted membership on request.  Similarly one may cancel the membership of club. First node is reserved for president of club and last node  is  reserved  for  secretary  of  club.  Write  C++  program  to  maintain  club  member‘s information using singlylinked list. Store student PRN and Name. Write functions to 
-	
-a)Add and delete the members as well as president or even secretary.
-b)Compute total number of members of club
-c)Display members 
-d)Display list in reverse order using recursion
-e)Two linked lists existsfor two divisions. Concatenate two lists.
+Name : Sahil Hadke
+Roll Number: 17
+Batch : S2
+
+Department of Computer Engineering has student's club named 'Pinnacle Club'.
+Students of Second, third and final year of department can be granted membership on request.
+Similarly one may cancel the membership of club.
+First node is reserved for president of club and last node  is  reserved  for  secretary  of  club.
+Write  C++  program  to  maintain  club  member‘s information using singlylinked list.
+Store student PRN and Name.
+	Write functions to 
+	a)Add and delete the members as well as president or even secretary.
+	b)Compute total number of members of club
+	c)Display members 
+	d)Display list in reverse order using recursion
+	e)Two linked lists existsfor two divisions. Concatenate two lists.
 */
 
 #include<iostream>
