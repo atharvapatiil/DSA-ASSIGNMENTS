09d9d8fe0f08832dda9198d454d26a20ea8db1d3
Sahil Hadke <51915783+sahilhadke@users.noreply.github.com>
2019-08-25 09:50:58 +0530

    Update Circular Linked List.cpp

--- Circular Linked List.cpp
+++ Circular Linked List.cpp
@@ -1,5 +1,20 @@
 /*
-The ticket booking system of Cinemax theater has to be implemented using C++ program. There are 10 rows and 7 seats in each row. Doubly circular linked list has to be maintained to keep track of free seats at rows. Assume some random booking to start with. Use array to store pointers (Head pointer) to each row.  On demand a) The list of available seats is to be displayedb) The seats are to be bookedc)  The booking can be cancelled.
+=====================================
+ ASSIGNMENT-8 : Circular Linked List
+=====================================
+
+Name : Sahil Hadke
+Roll No. : 17
+Batch : S2
+
+The ticket booking system of Cinemax theater has to be implemented using C++ program. 
+There are 10 rows and 7 seats in each row. 
+Doubly circular linked list has to be maintained to keep track of free seats at rows. 
+Assume some random booking to start with. 
+Use array to store pointers (Head pointer) to each row.  On demand 
+a) The list of available seats is to be displayed
+b) The seats are to be booked
+c)  The booking can be cancelled.
 */
 
 #include<iostream>
